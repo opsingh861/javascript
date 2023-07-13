22c7e44a1723f43d3a955df4f43395a728e4a2ef
opsingh861 <opsingh861@gmail.com>
2023-07-13 11:30:08 +0530

    dark mode

---
+++ DarkMode.js
@@ -0,0 +1,15 @@
+const body = document.querySelector("body")
+// console.log(body)
+
+const modeChange = () => {
+    // if (!body.classList.contains("dark")) {
+    //     body.classList.add("dark")
+    // } else {
+    //     body.classList.remove("dark")
+    // }
+
+    body.classList.toggle("dark") // toggle function is like switch its functioning as the same as the above line
+    console.log(body)
+}
+
+
